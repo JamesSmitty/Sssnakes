b17a9803b1449eb04f07b79bb443f27c078a82eb
JamesSmitty <mailtomaxsmith@gmail.com>
2017-05-22 20:27:02 -0400

    initial commit

---
+++ MergeSortTester.java
@@ -0,0 +1,108 @@
+//Sssnakez: James Smith, Dasha Shafrina, Jesse Sit
+//APCS2 pd3
+//HW07 -- What Does the Data Say?
+//2017-02-13
+
+/*======================================
+  class MergeSortTester
+
+  ALGORITHM:
+   First, the unsorted array is split in half until there's only one element in each of 
+  resulting arrays. So given an array with 4 elements, those 4 get split into two arrays
+  with 2 elements each, and each of those length 2 arrays get split into two arrays with
+  1 element each. In that case, there are four 1 element arrays. This guarantees sortedness
+  in each of the one element arrays. Now, each of those one element arrays are merged with 
+  the other one element array with which they were originally split, and the forming array
+  is merged with the other same-length array with which it was originally split, until 
+  there is one sorted array that is the length of the original unsorted array.x
+
+  BIG-OH CLASSIFICATION OF ALGORITHM:
+  O(nlog(n))
+
+  Mean execution times for dataset of size n:
+  Batch size: 100000
+  Maximum Random Number: 100000
+  n=1       time: 1.10E-08 seconds
+  n=10      time: 5.10E-07 seconds
+  n=100     time: 5.28E-06 seconds
+  n=1000    time: 7.4418E-5 seconds
+  n=10000   time: 7.96232E-4 seconds
+  n=100000  time: 0.00950558 seconds
+
+  ANALYSIS:
+  <INSERT YOUR RESULTS ANALYSIS HERE>
+  ======================================*/
+
+public class MergeSortTester 
+{
+
+    /******************************
+     * execution time analysis 
+    Our algo runs 100000 tests per randomly generated array sizes of 1,10, 100, and 100000. 
+	For each, the mean of the 100000 tests is calculated for an average execution time.
+	Method averageTestTime runs runTest 100000 times, which first generates a randomly-populated
+	array of a specified size and then starts timing the MergeSort. Once the array is sorted, the
+	total time is found by calculating the difference between the end and start time. Then the time
+	for the 100000 tests is averaged.
+     ******************************/
+
+    //generates an int array of length 'size' with random numbers within the range 0-maxNum
+    public static int[] generateArray(int size, int maxNum){
+	int[] retArr = new int[size];
+	for (int i = 0; i< size;i++){
+	    retArr[i] = (int)(Math.random()*maxNum);
+	}
+	return retArr;
+    }
+
+    //Return the execution time of the sorting algorithm in nano seconds
+    public static long runTest(int size, int maxNum){
+	int[] Arr = generateArray(size,maxNum);
+	long startTime = System.nanoTime();
+	MergeSort.sort(Arr);
+	long endTime   = System.nanoTime();
+	return endTime - startTime;
+    }
+
+    //returns the average execution time of the sorting algorithm over numTests iterations
+    public static double averageTestTime(int numTests, int size, int maxNum){
+	long totalTime = 0;
+	for(int i = 0; i < numTests; i++){
+	    totalTime += runTest(size, maxNum);
+	}
+	return (totalTime/(long)numTests)*0.000000001;
+    }
+
+    
+    public static void main( String[] args ){
+	int numTests = 100000;
+	int maxNum = 100000;
+	int size;
+	System.out.println("Tests of a merge sort algorithm at different sizes. Execution time is in seconds.\n Number of tests performed: " + numTests + "\n Maximum Number: " + maxNum);
+	//for n=1 ---------------------------------------
+	size = 1;
+	System.out.println("Now printing the average execution time for an array of size " + size);
+	System.out.println(averageTestTime(numTests, size, maxNum));
+	//for n=10 --------------------------------------
+	size = 10;
+	System.out.println("Now printing the average execution time for an array of size " + size);
+	System.out.println(averageTestTime(numTests, size, maxNum));
+	//for n=100 -------------------------------------
+	size = 100;
+	System.out.println("Now printing the average execution time for an array of size " + size);
+	System.out.println(averageTestTime(numTests, size, maxNum));
+	//for n=1000 ---------------------------------------
+	size = 1000;
+	System.out.println("Now printing the average execution time for an array of size " + size);
+	System.out.println(averageTestTime(numTests, size, maxNum));
+	//for n=10000 -------------------------------------
+	size = 10000;
+	System.out.println("Now printing the average execution time for an array of size " + size);
+	System.out.println(averageTestTime(numTests, size, maxNum));
+	//for n=100000 -------------------------------------
+	size = 100000;
+	System.out.println("Now printing the average execution time for an array of size " + size);
+	System.out.println(averageTestTime(numTests, size, maxNum));
+    }//end main
+
+}//end class
---
+++ README.md
@@ -0,0 +1,21 @@
+﻿# sssnakez
+Our testing methodology involves 3 methods. One to generate an array of a desired size populated with random integers from 0 to a desired upper bound, one to record the run time of the merge sort algorithm on a random array, and one to call the previous a certain number of times and average the runtimes out. By generating random arrays we are ensuring that we are not getting any special cases just by human selection. The importance of finding the average runtime is so that irregularities are ironed out over thousands of iterations. Such irregularities could be having a lack of computer resources during certain tests or starting up JDE. It is important to remember that the results of our tests vary computer to computer due to resources, so you may not have the same results, but the results should be fairly accurate relative to the system.
+To test our algorithm of merge sort against a BigOh classification we used a method of approximating execution time. In this method we took the average runtime of sorting a single element list. This should be constant which would allow us to apply it to the BigOh classifications in order to get an approximate runtime on a specific computer(James’ computer in this case). If you click on the link at the bottom you will be able to view the tables. Notice how O(nlogn) came the closest to the actual outcomes. It is important to remember that BigOh is a measure of complexity, not runtime. This means that true runtime is a more complex function than nlog(n) that involves error, however the difference due to error should average out to 0 over an adequate number of tests. The difference between the BigOh and the true runtime could be shrunk even more if we extended our algorithm analysis beyond the leading term. Our results from testing confirmed our hypothesis that merge sort was O(nlogn) after figuring out that it splits the list in half which leads to log(n) and then we run through a loop n times which gives us nlogn.
+
+Table data & 1st proof: https://docs.google.com/document/d/1lILfY4lx_79jLt3aA8igrNt5lVmWesSn8cCisKunBJk/pub
+
+Point of confusion in correlation Graphs (2nd proof):
+
+We plotted our calculated runtime values for each data set size against the expected runtime values for nlogn, n^2, and n. The calculate runtime values was the independent variable(x axis) while the expected runtime values were the dependent variable(y axis). We then calculated the lines of best fit for each graph. We looked for a graph with a slope closest to 1, since that would indicate a 1:1 correlation. A 1:1 correlation would mean that the actual run time is perfectly proportional to one of the expected run times. Based on the results of our first proof, we expected to have a 1:1 correlation to nlogn time but the slope was actually closer to 1 for n time. This was a point of confusion in our study. The graphs are below. 
+
+actual runtime vs nlogn runtime:
+
+![alt text](http://i66.tinypic.com/2a9dzz9.png "actual vs nlogn")
+
+actual runtime vs n^2 runtime:
+
+![alt text](http://i68.tinypic.com/2cpqr69.png "actual vs n^2")
+
+actual runtime vs n runtime:
+
+![alt text](http://i64.tinypic.com/2qbeoeo.png "actual vs n")
